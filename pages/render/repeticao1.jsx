899f76e21a54f350ed2fe73d6020dace83b038d1
“larissa.ribeiro” <larissa.ribeiro@g.globo>
2023-08-16 23:52:55 -0300

    Estrutura de repetição

---
+++ pages/render/repeticao1.jsx
@@ -0,0 +1,32 @@
+export default function Repeticao1() {
+
+    const listaAprovados = [
+        'Joao',
+        'Maria',
+        'Ana',
+        'Bia',
+        'Carlos',
+        'Daniel',
+        'Laura',
+    ]
+
+    function renderizarLista() {
+      return listaAprovados.map((nome, i) => <li key={i}>{nome}</li>)
+    }
+    return (
+        <ul>
+            {renderizarLista()}
+        </ul>
+    )
+}
+
+
+/*function renderizarLista() {
+    const itens = []
+
+    for (let i = 0; i < listaAprovados.length; i++) {
+        itens.push(<li key={i}>{listaAprovados[1]}</li>)
+
+    }
+    return itens
+}*/
